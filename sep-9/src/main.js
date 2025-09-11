7991a7ba53ad0b6a445c297636c584fa337918ec
BibhasShris <143662528+BibhasShris@users.noreply.github.com>
2025-09-11 17:03:55 -0600

    new stuff

--- sep-9/src/main.js
+++ sep-9/src/main.js
@@ -14,3 +14,23 @@ console.log(myClassroomInformation);
 
 import plustwo from "./myModuleDefaultExport.js";
 console.log(plustwo(7));
+
+import _ from "lodash";
+console.log(_.chunk(["a", "b", "c", "d"], 2));
+
+function search(value) {
+  console.log(value);
+}
+
+const searchBar = document.getElementById("search");
+searchBar.addEventListener("input", (e) => {
+  // search(e.target.value);
+  // debouncedSearch(e.target.value);
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => {
+    search(searchBar.value);
+    timeoutId = undefined;
+  }, 200);
+});
---
+++ sep-9/typescript-example/src/main.ts
@@ -0,0 +1,7 @@
+import "./style.css";
+import { plusOne } from "./myModule";
+
+let meaningOfLife: number = 42;
+console.log(meaningOfLife);
+
+console.log(plusOne(5));
